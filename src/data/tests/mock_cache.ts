703f2a811537719b0bae79a97375fc7d4ddc65f1
Felipe Santos <rfelipe273@gmail.com>
2021-12-28 00:11:01 -0300

    simplify CacheStore insertion with a replace method

--- src/data/tests/mock_cache.ts
+++ src/data/tests/mock_cache.ts
@@ -2,35 +2,39 @@ import { SavePurchases } from "@/domain/usecases";
 import { CacheStore } from "@/data/protocols/cache";
 
 export class CacheStoreSpy implements CacheStore {
-    messages: Array<CacheStoreSpy.Message> = [];
+    Actions: Array<CacheStoreSpy.Action> = [];
     deleteKey: string;
     insertKey: string;
     insertValues: Array<SavePurchases.Params>;
     delete(key: string): void {
         this.deleteKey = key;
-        this.messages.push(CacheStoreSpy.Message.delete);
+        this.Actions.push(CacheStoreSpy.Action.delete);
     }
     insert(key: string, value: any): void {
         this.insertKey = key;
         this.insertValues = value;
-        this.messages.push(CacheStoreSpy.Message.insert);
+        this.Actions.push(CacheStoreSpy.Action.insert);
+    }
+    replace(key: string, value: any): void {
+        this.delete(key);
+        this.insert(key, value);
     }
     simulateDeleteError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
-            this.messages.push(CacheStoreSpy.Message.delete);
+            this.Actions.push(CacheStoreSpy.Action.delete);
             throw new Error()
         });
     }
     simulateInsertError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
-            this.messages.push(CacheStoreSpy.Message.insert);
+            this.Actions.push(CacheStoreSpy.Action.insert);
             throw new Error()
         });
     }
 }
 
 export namespace CacheStoreSpy {
-    export enum Message {
+    export enum Action {
         delete,
         insert
     }
--- src/data/usecases/save_purchases/local_save_purchases.ts
+++ src/data/usecases/save_purchases/local_save_purchases.ts
@@ -4,8 +4,7 @@ export class LocalSavePurchases implements SavePurchases {
     constructor(private readonly cacheStore: CacheStore,
         private readonly timestamp: Date) { }
     async save(purchases: Array<SavePurchases.Params>): Promise<void> {
-        this.cacheStore.delete('purchases');
-        this.cacheStore.insert('purchases', {
+        this.cacheStore.replace('purchases', {
             timestamp: this.timestamp,
             value: purchases
         });
